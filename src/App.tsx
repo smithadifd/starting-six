3d0acfd115e47d0f01e235871c3f985ebc2663aa
smithadifd <as@retailarchitects.com>
2023-06-27 09:29:07 -0400

    Header components converted to TypeScript, fixed path resolution, and removed default components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import Counter from "./features/counter/Counter";
+// import Layout from "components/ui/Layout";
+import Layout from "./components/ui/Layout";
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <Counter />
+      <Layout />
     </div>
   );
 }
